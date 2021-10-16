205719a38f7867755d3247726b33089e2d97657b
Faizan Siddiqui <siddiquimdfaizan82@gmail.com>
2021-10-17 00:52:20 +0500

    ui changes

--- src/Components/vacanciesCard.jsx
+++ src/Components/vacanciesCard.jsx
@@ -1,18 +1,48 @@
 import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
 import { Button } from '@material-ui/core';
 import { Card } from '@mui/material';
 import React from 'react';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import DialogContent from '@material-ui/core/DialogContent';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        [theme.breakpoints.down("sm")]: {
+            padding: "0",
+        },
+    },
+    // root: {
+    //     [theme.breakpoints.down("sm")]: {
+    //         padding: "0",
+    //     }
+    // },
+    main: {
+        display: "flex",
+        justifyCcontent: "space-between"
+    },
+    mainChild: {
+        display: "flex",
+        alignItems: "center",
+    },
+    companyLogo: {
+        width: "65px",
+        height: "65px",
+        margin: "10px 10px 10px 0",
+        borderRadius: "50%",
+        overflow: "hidden",
+        display: "inline-block",
+    }
+}));
 export default function VacanciesCard({ campusData }) {
+    const classes = useStyles();
     return (
         <div>
             <Card style={{ margin: "10px", padding: "10px" }}>
-                <DialogContent >
-                    <div className="main">
-                        <div className="mainChild">
-                            <div className="companyLogo">
+                <DialogContent classes={{ root: classes.root }} >
+                    <div className={classes.main}>
+                        <div className={classes.mainChild}>
+                            <div className={classes.companyLogo}>
                                 <img src="https://1.bp.blogspot.com/-QEJHSCZ29Z8/XILObl4OjnI/AAAAAAAAIfo/eA1agAsDotIVQDHl9h7pJ9hnvLOHF2U8ACK4BGAYYCw/s1600/icon%2Bslack%2Bvector.png" alt="company logo" />
                             </div>
                             <div className="cName">
