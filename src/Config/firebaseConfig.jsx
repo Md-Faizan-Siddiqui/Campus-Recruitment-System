356ac7427323616099d2531575991c53ce506c0a
Faizan Siddiqui <siddiquimdfaizan82@gmail.com>
2021-11-09 04:44:41 +0500

    Protect Credentials

--- src/Config/firebaseConfig.jsx
+++ src/Config/firebaseConfig.jsx
@@ -3,17 +3,23 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database'
 import 'firebase/storage';
-
   const firebaseConfig = {
-    apiKey: "AIzaSyBmCSBnamp2RwymF2ctIM-Ut3tGjppMfcE",
-    authDomain: "campus-recruitment-syste-3c034.firebaseapp.com",
-    databaseURL: "https://campus-recruitment-syste-3c034-default-rtdb.firebaseio.com",
-    projectId: "campus-recruitment-syste-3c034",
-    storageBucket: "campus-recruitment-syste-3c034.appspot.com",
-    messagingSenderId: "865674085866",
-    appId: "1:865674085866:web:a4f039b807132123a45e93",
-    measurementId: "G-EG6XF34MV2"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURMENT_ID,
   };
+  
+  
+  
+  
+  
+  
+  
 
 
 firebase.initializeApp(firebaseConfig);
